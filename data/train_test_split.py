a60208001c7c6869335fe702a1a3c02e469771e0
Calvin C. K. Yeung <70192523+calvinyeungck@users.noreply.github.com>
2023-07-05 03:10:50 +0900

    train_test_split.py

--- data/train_test_split.py
+++ data/train_test_split.py
@@ -4,12 +4,12 @@ import pdb
 from sklearn.model_selection import train_test_split
 
 
-df=pd.read_csv("/home/c_yeung/workspace6/python/project3/data/dataset.csv")
+df=pd.read_csv("dataset.csv")
 train, test = train_test_split(df, test_size=0.2, stratify=df["shot_outcome_grouped"], random_state=22) 
 
 print(len(train),len(test))
 
-train.to_csv("/home/c_yeung/workspace6/python/project3/data/train.csv",index=False)
-test.to_csv("/home/c_yeung/workspace6/python/project3/data/test.csv",index=False)
+train.to_csv("train.csv",index=False)
+test.to_csv("test.csv",index=False)
 
 # pdb.set_trace()
